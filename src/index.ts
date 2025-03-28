4a238a1b5f83de634c944080d00240f323fb484d
Arifcodes11 <arifcodes11@gmail.com>
2025-03-28 14:12:20 +0530

    update server log message format to uppercase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { allRoutes } from "./routes/routes";
 import { serve } from "@hono/node-server";
+import { allRoutes } from "./routes/routes";
 
 serve(allRoutes, (info) => {
-  console.log(`Server is running @ http://localhost:${info.port}`);
+  console.log(`SERVER RUNNING ON PORT ${info.port}`);
 });
