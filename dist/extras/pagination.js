9945154c54075e48955fe0660c99c34a74e0fc44
Arif  A <arifcodes11@gmail.com>
2025-04-05 14:15:26 +0530

    Delete dist directory

--- dist/extras/pagination.js
+++
@@ -1,6 +0,0 @@
-export const getPagination = (page, limit) => {
-    const currentPage = Math.max(page, 1); // Ensures page is at least 1
-    const perPage = Math.max(limit, 1); // Ensures limit is at least 1
-    const skip = (currentPage - 1) * perPage;
-    return { skip, take: perPage };
-};
