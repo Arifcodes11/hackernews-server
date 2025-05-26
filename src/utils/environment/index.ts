64f2bf4ab6c529b30810c5aa299e1359243ffd1d
Arifcodes11 <arifcodes11@gmail.com>
2025-05-26 13:30:46 +0530

    fix: remove Google client secret and ID from environment variables

--- src/utils/environment/index.ts
+++ src/utils/environment/index.ts
@@ -3,8 +3,6 @@ export const serverUrl = process.env.SERVER_URL!;
 export const betterAuthSecret = process.env.BETTER_AUTH_SECRET!;
 
 export const webClientUrl = process.env.WEB_CLIENT_URL!;
-export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET!; 
-export const googleClientId = process.env.GOOGLE_CLIENT_ID!;
 export const pineconeApiKey = process.env.PINECONE_API_KEY!;
 export const openaiApiKey = process.env.OPENAI_API_KEY!;
 export const mistralApiKey = process.env.MISTRAL_API_KEY!;
